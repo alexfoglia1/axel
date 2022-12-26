8bdf77b4dbb9155b9d3e421e0bc1df7721ea82fc
Alex Foglia <alex.f1793@gmail.com>
2022-12-26 17:44:23 +0100

    Enabled paging

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -10,7 +10,7 @@
 #define STAGE_V 'b'
 
 void
-_main(multiboot_info_t* mbd, uint32_t magic)
+kernel_main(multiboot_info_t* mbd, uint32_t magic)
 {
 	errno = ENOERR;
 	tty_init();
@@ -22,24 +22,24 @@ _main(multiboot_info_t* mbd, uint32_t magic)
 	{
 		printf("%s\n\n", "Bootloader recognized, booting the system");
 
-		uint64_t memsize = 0;
-    	for(uint32_t i = 0; i < mbd->mmap_length; i += sizeof(multiboot_memory_map_t)) 
-    	{
-        	multiboot_memory_map_t* mmmt = 
-            	(multiboot_memory_map_t*) (mbd->mmap_addr + i);
+		//uint64_t memsize = 0;
+    	//for(uint32_t i = 0; i < mbd->mmap_length; i += sizeof(multiboot_memory_map_t)) 
+    	//{
+        // 	multiboot_memory_map_t* mmmt = 
+        //    	(multiboot_memory_map_t*) (mbd->mmap_addr + i);
  
-			bool available = (mmmt->type == MULTIBOOT_MEMORY_AVAILABLE);
-			tty_set_color(available ? VGA_COLOR_GREEN : VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+		//	bool available = (mmmt->type == MULTIBOOT_MEMORY_AVAILABLE);
+		//	tty_set_color(available ? VGA_COLOR_GREEN : VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
 
-        	printf("Addr : %x   \t  Length : %x \t  Type : %u\n", mmmt->addr, mmmt->len, mmmt->type);
+        //	printf("Addr : %x   \t  Length : %x \t  Type : %u\n", mmmt->addr, mmmt->len, mmmt->type);
 
-			if (available)
-			{
-				memsize += mmmt->len;
-			}
-    }
+		//	if (available)
+		//	{
+		//		memsize += mmmt->len;
+		//	}
 
-	printf("\nTotal available RAM: %U KiB\n", memsize / 1024);
+		uint64_t memsize = 0;
+		printf("\nTotal available RAM: %U KiB\n", memsize / 1024);
 	}
 	else
 	{
