22164307ac103b3b5eaedbdf0a37f2a96a93c786
Alex Foglia <alex.f1793@gmail.com>
2022-12-26 19:12:16 +0100

    debug projects includes common

--- debug/debug/main.c
+++ debug/debug/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
+#include <stdint.h>
 
 int
 main(int argc, char** argv)
 {
     /** Call and debug kernel functions which are bugged :) **/
 
-    printf("floating point(%f)\n", -123.456789123);
+    uint64_t hex = 0xCAFEBABEDEAD;
+    printf("hex 0x%X", hex);
 }
