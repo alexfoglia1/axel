636f08273be0276dabc59d7ea32f74cd4951ab5f
Alex Foglia <alex.f1793@gmail.com>
2023-01-24 01:23:08 +0100

    removed syscall interface from asm.h

--- kernel/include/kernel/arch/asm.h
+++ kernel/include/kernel/arch/asm.h
@@ -14,7 +14,6 @@ void enable_paging();
 void copy_page_physical(uint32_t src, uint32_t dst);
 void load_page_directory(uint32_t page_directory);
 void context_switch(uint32_t eip, uint32_t esp, uint32_t ebp, uint32_t page_directory);
-void syscall_interface(uint32_t syscall_int_no, uint32_t syscall_type, uint32_t buffer, uint32_t count, uint32_t extra);
 void enter_user_mode();
 
 void __attribute__((noreturn)) panic(const char* prompt); // Preferred abnormal termination if an exception happened in kernel mode, otherwise user mode shall call the stdlib abort()
