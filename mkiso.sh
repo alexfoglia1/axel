054c067899a367334d0804be08a80cb0ad05aa60
Alex Foglia <alex.f1793@gmail.com>
2023-01-31 00:36:21 +0100

    mkiso creates initrd

--- mkiso.sh
+++ mkiso.sh
@@ -1,25 +1,40 @@
 #!/bin/sh
-set -e
 . ./env-build.sh
+set -e
 
 echo "Generating $OSNAME iso . . ."
+
 if [ ! -f sysroot/boot/$OSNAME.kernel ]; then
   echo "Could not find $OSNAME binaries, make sure you have compiled them correctly"
   exit 1
 fi
+
+echo "Generating $OSNAME.initrd"
+
+if [ ! -f make-initrd/mkinitrd ]; then
+  echo "Compiling mkinitrd"
+  gcc make-initrd/make-initrd.c -o make-initrd/mkinitrd
+else
+  echo "Found mkinitrd"
+fi
+
+cd make-initrd
+./mkinitrd $SYSROOT
+cd ..
 
 mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
 cp sysroot/boot/$OSNAME.kernel isodir/boot/$OSNAME.kernel
-cp sysroot/boot/$OSNAME.initrd isodir/boot/$OSNAME.initrd
+mv make-initrd/initrd.img isodir/boot/$OSNAME.initrd
+
 cat > isodir/boot/grub/grub.cfg << EOF
 menuentry "$OSNAME" {
 	multiboot /boot/$OSNAME.kernel
-        module /boot/$OSNAME.initrd
+        module /boot/$OSNAME.initrd.gz
 }
 EOF
 grub-mkrescue -o $OSNAME.iso isodir
 
-echo "Generated ${PWD}/$OSNAME.iso"
+echo "Generat/ed ${PWD}/$OSNAME.iso"
